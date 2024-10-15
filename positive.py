669a5e9bb51ca824bd974733814db2108ff79d00
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-15 14:05:03 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,10 @@
+'''
+Author:Nandana Hasheed
+Date:15-10-2024
+Python program to check whether a given number is positive or negative
+'''
+number=int(input("Enter a number :"))
+if number>0:
+ print("The given number:",number,"is positive")
+else:
+    print("The given number :",number,"is negative")
