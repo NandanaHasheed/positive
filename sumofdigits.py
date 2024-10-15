a37f8810813eeb1fb936067684850d975f28a283
NandanaHasheed <nandanahasheed2028@cs.sjcetpalai.ac.in>
2024-10-15 16:27:02 +0530

    Update sumofdigits.py

--- sumofdigits.py
+++ sumofdigits.py
@@ -1,7 +1,11 @@
+'''
+Author : Nandana Hasheed 
+DAte: 15-10-2024
+Python program to find the sum of digits 
 num=int(input("Enter number"))
 sum=0
 while(num>0):
     r=num%10
     sum=sum+r
     num=num//10
-print(sum)
+print("Sum of digits",sum)
